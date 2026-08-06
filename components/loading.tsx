b5b21470d894151f061ee14a7649fd256818a8cc
Munachi | Developer | Designer <munachinedu4jesus123@gmail.com>
2026-08-06 15:35:24 +0100

    Add TypeScript annotation for scale prop
    Added type annotation for the scale prop in Loading component.

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,7 +1,7 @@
 
 
-
-export default function Loading({scale}) {
+// This component was not eventually used
+export default function Loading({scale} : number) {
 	return(
 		<div className={`m-auto loading p-${scale} rounded-full flex`} />
 	)
